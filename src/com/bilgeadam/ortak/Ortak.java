7388e1b70794dc5965a7ea5e8fbd1a6902004661
samet.coruk <sametcoruk0604@gmail.com>
2021-11-21 14:39:46 +0300

    harf çevirme ve tarih metodları eklendi.

---
+++ src/com/bilgeadam/ortak/Ortak.java
@@ -0,0 +1,36 @@
+package com.bilgeadam.ortak;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Ortak {
+
+    private static Locale localeTR = new Locale("tr", "TR");
+
+    public static String buyukHarfeCevir(String p_kelime)
+    {
+        return p_kelime.toUpperCase(localeTR);
+    }
+
+    public static String kucukHarfeCevir(String p_kelime)
+    {
+        return p_kelime.toLowerCase(localeTR);
+    }
+
+    public static String tarihToStr(Date p_tarih)
+    {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        return sdf.format(p_tarih);
+    }
+
+    public static Date strToTarih(String p_tarih) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        return sdf.parse(p_tarih);
+    }
+
+
+
+
+}
